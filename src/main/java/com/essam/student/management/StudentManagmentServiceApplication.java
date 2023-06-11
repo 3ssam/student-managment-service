8a3240a897cb6ebc935530e853b2b62b44e49835
mohamed-essam <mohamed.essam.mohamed.kamel@gmail.com>
2023-06-11 10:02:05 +0300

    init project

---
+++ src/main/java/com/essam/student/management/StudentManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.essam.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagmentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/essam/student/management/StudentManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essam.student.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
